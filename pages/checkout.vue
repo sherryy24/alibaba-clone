74f2cb7c9de5c5bd14c18000e189926b1d26bebe
sherryy24 <gshashwat24@gmail.com>
2024-05-15 11:28:40 +0530

    Address and auth page added

---
+++ pages/checkout.vue
@@ -0,0 +1,209 @@
+<template>
+    <MainLayout>
+        <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+  
+            <div class="md:flex gap-4 justify-between mx-auto w-full">
+                <div class="md:w-[65%]">
+                    <div class="bg-white rounded-lg p-4">
+
+                        <div class="text-xl font-semibold mb-2">Shipping Address</div>
+
+                        <div v-if="false">
+                            <NuxtLink 
+                                to="/address"
+                                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+                            >
+                                <Icon name="mdi:plus" size="18" class="mr-2"/>
+                                Update Address
+                            </NuxtLink>
+
+                            <div class="pt-2 border-t">
+                                <div class="underline pb-1">Delivery Address</div>
+                                <ul class="text-xs">
+                                    <li class="flex items-center gap-2">
+                                        <div>Contact name:</div> 
+                                        <div class="font-bold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Address:</div> 
+                                        <div class="font-bold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Zip Code:</div> 
+                                        <div class="font-bold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>City:</div> 
+                                        <div class="font-bold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Country:</div> 
+                                        <div class="font-bold">Test</div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <NuxtLink 
+                            v-else
+                            to="/address"
+                            class="flex items-center text-blue-500 hover:text-red-400"
+                        >
+                            <Icon name="mdi:plus" size="18" class="mr-2"/>
+                            Add New Address
+                        </NuxtLink>
+                    </div>
+
+                    <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+                        <div v-for="product in products" :key="product">
+                            <CheckoutItem :product="product" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="md:hidden block my-4"/>
+                <div class="md:w-[35%]">
+                    <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+
+                        <div class="text-2xl font-extrabold mb-2">Summary</div>
+
+                        <div class="flex items-center justify-between my-4">
+                            <div class="">Total Shipping</div>
+                            <div class="">Free</div>
+                        </div>
+
+                        <div class="border-t" />
+
+                        <div class="flex items-center justify-between my-4">
+                            <div class="font-semibold">Total</div>
+                            <div class="text-2xl font-semibold">
+                                $ <span class="font-extrabold">{{ total / 100 }}</span>
+                            </div>
+                        </div>
+
+                        <form @submit.prevent="pay()">
+                            <div 
+                                class="border border-gray-500 p-2 rounded-sm" 
+                                id="card-element" 
+                            />
+
+                            <p 
+                                id="card-error" 
+                                role="alert" 
+                                class="text-red-700 text-center font-semibold" 
+                            />
+
+                            <button 
+                                :disabled="isProcessing"
+                                type="submit"
+                                class="
+                                mt-4
+                                    bg-gradient-to-r 
+                                  from-[#FE630C] 
+                                  to-[#FF3200]
+                                    w-full 
+                                    text-white 
+                                    text-[21px] 
+                                    font-semibold 
+                                    p-1.5 
+                                    rounded-full
+                                "
+                                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+                            >
+                                <Icon v-if="isProcessing" name="eos-icons:loading" />
+                                <div v-else>Place order</div>
+                            </button>
+                        </form>
+
+                    </div>
+
+                    <div class="bg-white rounded-lg p-4 mt-4">
+                        <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
+                        <p class="my-2">
+                            AliExpress keeps your information and payment safe
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore();
+const route = useRoute()
+
+let stripe = null
+let elements = null
+let card = null
+let form = null
+let total = ref(0)
+let clientSecret = null
+let currentAddress =ref(null)
+let isProcessing = ref(false)
+
+onMounted(()=>{
+    isProcessing.value = true;
+
+    userStore.checkout.forEach(item => {
+        total.value += item.price
+
+        
+    });
+})
+
+
+watch(()=> total.value , ()=>{
+    if(total.value>0){
+        stripeInit()
+    }
+})
+
+const pay = async ()=>{
+
+}
+
+const createOrder= async (stripeId) =>{
+
+}
+
+const showError = async (errorMsgText)=>{
+
+}
+
+const stripeInit = async ()=>{
+
+}
+const products = [
+    {
+        "id": 1,
+        "name": "Product 1",
+        "title": "title1",
+        "price": 10.99,
+        "description": "This is the description of Product 1.",
+        "url": "https://picsum.photos/200/200"
+    },
+    {
+        "id": 2,
+        "name": "Product 2",
+        "title": "title2",
+        "price": 11.99,
+        "description": "This is the description of Product 2.",
+        "url": "https://picsum.photos/200/200"
+    },
+    {
+        "id": 3,
+        "name": "Product 3",
+        "title": "title3",
+        "price": 12.99,
+        "description": "This is the description of Product 3.",
+        "url": "https://picsum.photos/200/200"
+    }
+]
+</script>
+
--- pages/shoppingcart.vue
+++ pages/shoppingcart.vue
@@ -131,6 +131,43 @@ const cards = ref([
 onMounted(()=>{
     setTimeout(()=> userStore.isLoading = false, 200)
 })
+
+const totalPriceComputed = computed(()=>{
+    let price=0;
+    userStore.cart.forEach(prod =>{
+        price += prod.price;
+    })
+    return price/100
+})
+
+const selectedRadioFunc =(e)=>{
+    if(!selectedArray.value.length){
+        selectedArray.value.push(e)
+        return
+    }
+    selectedArray.value.forEach((item , index)=>{
+        if(e.id != item.id){
+            selectedArray.value.push(e)
+        }else{
+            selectedArray.value.splice(index,1)
+        }
+    })
+}
+
+const goToCheckout = ()=>{
+    let ids = []
+    userStore.checkout =[];
+
+    selectedArray.value.forEach(item => ids.push(item.id))
+
+    let res = userStore.cart.filter((item)=>{
+        return ids.indexOf(item.id) != -1
+    })
+
+    res.forEach(item => userStore.checkout.push(toRaw(item)))
+
+    return navigateTo('/checkout')
+}
 const products = [
     {
         "id": 1,
